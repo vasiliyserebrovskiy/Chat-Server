35eca856752fc8f5cfe74388a2ce3cf1acce6fcf
SerebrovskiyV <vasiliy.serebrovskiy@gmail.com>
2025-06-16 13:25:37 +0200

    server almost done

--- src/ait/socket/chat/ChatServerAppl.java
+++ src/ait/socket/chat/ChatServerAppl.java
@@ -1,29 +1,33 @@
-package ait.socket.server;
+package ait.socket.chat;
 
 
 
-import ait.socket.server.task.ClientHandler;
+import ait.socket.chat.task.ChatServerReceiver;
+import ait.socket.chat.task.ChatServerSender;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * @author Vasilii Serebrovskii
  * @version 1.0 (16.06.2025)
  */
-public class SicketServerAppl {
+public class ChatServerAppl {
     public static void main(String[] args) throws InterruptedException {
 
         int port = 9000; // кого слушаем
+
+        BlockingQueue<String> messageBox = new ArrayBlockingQueue<>(10);
+
+        // Daemon for sender
+        ChatServerSender sender = new ChatServerSender(messageBox);
+        Thread senderThread = new Thread(sender);
+        senderThread.setDaemon(true);
+        senderThread.start();
+
+
         ExecutorService executorService = Executors.newFixedThreadPool(2);
         //сервер должен быть постоянно запущен и ждать, что кто-то к нему обратиться
         try (ServerSocket serverSocket = new ServerSocket(port);) {
@@ -33,7 +37,10 @@ public class SicketServerAppl {
                 // в состоянии accept
                 System.out.println("Connection established");
                 System.out.println("Client host:" + socket.getInetAddress() + " : " + socket.getPort());
-                executorService.execute(new ClientHandler(socket));
+//                executorService.execute(new ClientHandler(socket));
+                sender.addClient(socket); // добавили PrintWriter
+                ChatServerReceiver receiver = new ChatServerReceiver(socket, messageBox); // create receiver
+                executorService.execute(receiver);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
